c39a2b3a6cd7d34ffdac7341b1cd484269080dbe
kmhkyle <43160736+kmhkyle@users.noreply.github.com>
2021-12-19 19:32:35 -0500

    Update Day_15.py
    WIP update 2

--- Day_15.py
+++ Day_15.py
@@ -1,34 +1,73 @@
+from collections import defaultdict
 f = open('Day_15_Input.txt', 'r')
 grid = f.read().split('\n')
 
 cavernWidth = len(grid[0])
 cavernLength = len(grid)
-print(cavernLength, cavernWidth)
+vertDistances = {(0, 0): 0}
+adjacentVerts = {(0, 0): []}
 
-def checkNorth(Y, X):
-    return grid[Y - 1][X]
-
-
-
-def findRisk(currentY, currentX, visitedDistance):
-    #edge cases
-    if currentY == 0:
-        if currentX == 0:
-            pass
-        elif currentX == cavernWidth - 1:
-            pass
+def findAdjacent(currentAdjacents):
+    updatedAdj = defaultdict(list)
+    for vert in currentAdjacents.keys():
+        Y = vert[0]
+        X = vert[1]
+        if Y == 0:
+            if X == 0:
+                checkEast(Y, X, updatedAdj)
+                checkSouth(Y, X, updatedAdj)
+            elif X == cavernWidth - 1:
+                checkWest(Y, X, updatedAdj)
+                checkSouth(Y, X, updatedAdj)
+            else:
+                checkEast(Y, X, updatedAdj)
+                checkSouth(Y, X, updatedAdj)
+                checkWest(Y, X, updatedAdj)
+        elif Y == cavernLength - 1:
+            # dont need to check right, bottom edge since thats where we want to be
+            if X == 0:
+                checkNorth(Y, X, updatedAdj)
+                checkEast(Y, X, updatedAdj)
+            else:
+                checkNorth(Y, X, updatedAdj)
+                checkEast(Y, X, updatedAdj)
+                checkWest(Y, X, updatedAdj)
+        elif X == 0:
+            checkNorth(Y, X, updatedAdj)
+            checkEast(Y, X, updatedAdj)
+            checkSouth(Y, X, updatedAdj)
+        elif X == cavernWidth - 1:
+            checkNorth(Y, X, updatedAdj)
+            checkWest(Y, X, updatedAdj)
+            checkSouth(Y, X, updatedAdj)
         else:
-            pass
+            checkNorth(Y, X, updatedAdj)
+            checkEast(Y, X, updatedAdj)
+            checkSouth(Y, X, updatedAdj)
+            checkWest(Y, X, updatedAdj)         
+    currentAdjacents = updatedAdj
+    return currentAdjacents
 
+def checkNorth(Y, X, dict):
+    if (Y - 1, X) not in vertDistances:
+        return dict[(Y, X)].append((Y - 1, X))
 
+def checkEast(Y, X, dict):
+    if (Y, X + 1) not in vertDistances:
+        return dict[(Y, X)].append((Y, X + 1))
 
+def checkSouth(Y, X, dict):
+    if (Y + 1, X) not in vertDistances:
+        return dict[(Y, X)].append((Y + 1, X))
 
+def checkWest(Y, X, dict):
+    if (Y, X - 1) not in vertDistances:
+        return dict[(Y, X)].append((Y, X - 1))
 
-    for y in range(cavernLength):
-        for x in range(cavernWidth):
-            print(grid[y][x])
-
-
-
-print(checkNorth(1,2))
-    
+adjacentVerts = findAdjacent(adjacentVerts)
+for k, v in adjacentVerts.items():
+    keyDist = vertDistances[k]
+    for vert in v:
+        print(vert)
+        print(grid[vert[0]][vert[1]])
+        
